2162517964d33b795acbb4ec0977a82d43569ac3
Mr_Whoopee <artem.holovko.97@gmail.com>
2026-03-12 13:50:52 +0200

    done with second hw

---
+++ src/controllers/notesController.js
@@ -0,0 +1,36 @@
+import { Note } from '../models/note.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { ensureNoteExists } from '../utils/ensureNoteExists.js';
+
+export const getAllNotes = ctrlWrapper(async (req, res) => {
+  const notes = await Note.find();
+  ensureNoteExists(notes);
+  res.status(200).json(notes);
+});
+
+export const getNoteById = ctrlWrapper(async (req, res) => {
+  const { noteId } = req.params;
+  const note = await Note.findById(noteId);
+  ensureNoteExists(note);
+  res.status(200).json(note);
+});
+
+export const createNote = ctrlWrapper(async (req, res) => {
+  const note = await Note.create(req.body);
+
+  res.status(201).json(note);
+});
+
+export const updateNote = ctrlWrapper(async (req, res) => {
+  const { noteId } = req.params;
+  const note = await Note.findByIdAndUpdate(noteId, req.body, { new: true });
+  ensureNoteExists(note);
+  res.status(200).json(note);
+});
+
+export const deleteNote = ctrlWrapper(async (req, res) => {
+  const { noteId } = req.params;
+  const note = await Note.findByIdAndDelete(noteId);
+  ensureNoteExists(note);
+  res.status(200).json(note);
+});
--- src/server.js
+++ src/server.js
@@ -1,60 +1,27 @@
 import express from 'express';
 import cors from 'cors';
-import pino from 'pino-http';
 import 'dotenv/config';
+import { connectMongoDB } from './db/connectMongoDB.js';
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import notesRoutes from './routes/notesRoutes.js';
 
 const app = express();
 
 const PORT = process.env.PORT ?? 3000;
 
+app.use(logger);
 app.use(express.json());
 app.use(cors());
-app.use(
-  pino({
-    level: 'info',
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'HH:MM:ss',
-        ignore: 'pid,hostname',
-        messageFormat:
-          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-        hideObject: true,
-      },
-    },
-  }),
-);
 
-app.get('/notes', (req, res) => {
-  res.json({
-    message: 'Retrieved all notes',
-  });
-});
+app.use(notesRoutes);
 
-app.get('/notes/:noteId', (req, res) => {
-  const id_param = req.params.noteId;
-  res.json({
-    message: `Retrieved note with ID: ${id_param}`,
-  });
-});
+app.use(notFoundHandler);
 
-app.get('/test-error', () => {
-  throw new Error('Simulated server error');
-});
+app.use(errorHandler);
 
-app.use((req, res) => {
-  res.status(404).json({
-    message: 'Route not found',
-  });
-});
-
-app.use((err, req, res, next) => {
-  console.error('Error:', err.message);
-  res.status(500).json({
-    message: err.message,
-  });
-});
+await connectMongoDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
